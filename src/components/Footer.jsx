92f742f14084bc4d1e7e6ee40243b2ce9cc45526
John Mark Tizado <johnmarktizado@gmail.com>
2024-03-19 19:30:56 +0800

    add: first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-icons">
+        <a href="https://www.linkedin.com/in/johnmarktizado/" target="_blank">
+          <img src="./LinkedIn.png" className="icons" />
+        </a>
+        <a href="https://github.com/ProgrammerJM" target="_blank">
+          <img src="./GitHub.png" className="icons" />
+        </a>
+        <a href="https://www.instagram.com/jmjtiz/" target="_blank">
+          <img src="./Instagram.png" className="icons" />
+        </a>
+      </div>
+      <div className="footer-me">
+        Made with {`<`}3 by<a className="myName">jtiz</a>
+      </div>
+    </footer>
+  );
+}
